b7fa3bb263be87e2d947fc982a6bded83d4b3e98
bigl162 <djinterrante@gmail.com>
2018-06-10 11:47:06 -0400

    Initial commit

---
+++ AssumeBot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import praw
+import pdb
+import re
+import os
+
+reddit = praw.Reddit('sleet_bot_1')
+
+subreddit = reddit.subreddit("learnpython")
+
+for submission in subreddit.hot(limit=5):
+    print("Title: ", submission.title)
+    print("Text: ", submission.selftext)
+    print("Score: ", submission.score)
+    print("---------------------------------\n")
---
+++ PracticeScript.py
@@ -0,0 +1,18 @@
+import praw
+import pdb
+import re
+import os
+
+reddit = praw.Reddit('sleet_bot_1')
+
+subreddit = reddit.subreddit("learnpython")
+
+
+if not os.path.isfile("C:\\Users\\Dan\\PycharmProjects\\RedditBot1\\posts_replied_to.txt"):
+    posts_replied_to = []
+
+else:
+    with open("posts_replied_to.txt", "r") as f:
+       posts_replied_to = f.read()
+       posts_replied_to = posts_replied_to.split("\n")
+       posts_replied_to = list(filter(None, posts_replied_to))
